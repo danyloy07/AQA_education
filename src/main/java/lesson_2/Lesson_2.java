44e716ef590ceeb996a8cb5555c27bcb32cc1b5c
danyloy07 <iakimets.danil@gmail.com>
2025-01-16 13:19:37 +0100

    add ternar scanner

--- src/main/java/lesson_2/Lesson_2.java
+++ src/main/java/lesson_2/Lesson_2.java
@@ -63,10 +63,21 @@ public class Lesson_2 {
         }
     }
 
+    //вложеннй тернарный оператор
+    public static void ternarExample(){
+        Scanner ternarScanner = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = ternarScanner.nextInt();
+        String message = age <18 ? "you are less than 18" : age > 30 ? "You are more than 30":"You are beetwen 18 and 30";
+        System.out.println(message);
+        ternarScanner.close();
+    }
+
     public static void main(String[] args) {
         scanner();
         switchCase();
         ifElse();
+        ternarExample();
     }
 }
 
